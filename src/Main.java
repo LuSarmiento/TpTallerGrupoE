47063bb4e6b8bf92caf905572761079ec6f3e416
enry0689 <103335948+enry0689@users.noreply.github.com>
2022-11-14 17:40:41 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola, bebitos hermochos");
+        System.out.println("hola, bebitos hermochos Editado beibi");
     }
 }
