e5587ba0eb9b5d5a2e444aa902d2ad1c240d508d
Aly <91966204+damarisrios@users.noreply.github.com>
2022-11-14 17:41:46 -0300

    hola, bebitos hermochos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hola, bebitos hermochos Editado beibi");
+        System.out.println("hola, bebitos hermochos");
     }
 }
