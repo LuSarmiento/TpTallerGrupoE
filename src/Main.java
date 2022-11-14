8502155b1ef39fe8df736b466aeb92977ed583ba
s7ef <103335856+s7ef@users.noreply.github.com>
2022-11-14 18:12:05 -0300

    escribir archivo de prueba

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("hola, bebitos hermochos");
+    public static void main(String[] args) throws IOException {
+        final String archivo = "test.txt";
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(archivo));
+
+        bw.write("test\n");
+
+        bw.close();
     }
 }
