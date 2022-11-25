b75e50e50df5cb56162a690a1c8bb29683409a8b
s7ef <103335856+s7ef@users.noreply.github.com>
2022-11-25 19:42:04 -0300

    borrar prints y writes innecesarios

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,13 @@ import sorteo.Apuesta;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner entrada = new Scanner(System.in);
-
         String date = java.time.LocalDateTime.now().toString()
                 .split("T")[0];
-        System.out.println("fecha " + date);
-
-        System.out
-                .println("Desea apostar para la mañana o para la noche\nIngrese 1 para 'DIA'\nIngrese 2 para 'NOCHE'");
-        int turno = entrada.nextInt();
+        String turno = "maniana";
 
         List<Apuesta> apuestasList = new ArrayList<Apuesta>();
         for (int i = 0; i < 4; i++) {
             Apuesta apuesta = new Apuesta();
-            System.out.println("valor generado: " + apuesta.getValor());
-            System.out.println("numero generado: " + apuesta.getNumero());
-            System.out.println("posicion generado: " + apuesta.getPosicion());
             apuestasList.add(apuesta);
         }
 
@@ -30,14 +21,6 @@ public class Main {
 
         BufferedWriter bw = new BufferedWriter(new FileWriter(nombreArchivo));
 
-        bw.write("Nombre del archivo=" + nombreArchivo);
-        bw.newLine();
-        bw.write("Jugada= " + turno);
-        bw.newLine();
-        bw.write("Fecha y Hora=" + date);
-        bw.newLine();
-        bw.write("Dni=32541874");
-        bw.newLine();
         bw.write("Apuesta Numero Posicion\n");
         for (Apuesta elemento : apuestasList) {
             bw.write("$" + elemento.getValor() + " " + elemento.getNumero() + " " + elemento.getPosicion() + "\n");
